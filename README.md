d4daf3f4fa11e5b65048a917218ec59fcc8fe0b9
Kevin Johnson <kevin@coolkev.com>
2023-09-25 10:01:25 -0400

    Prometheus conversion

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
 # weatherflow-tempest-influxdb-integration
 
-Pipes data from a Weatherflow Tempest into an influx server.
+Exports tempest data in a Prometheus exporter endpoint.
 
-Supports InfluxDB 2.0 as that's what I'm running. Influx 1.8 also has an easy integration with Node so if you fork this you'd probably be able to update it to use that instead, it's just a different library and slightly different functions in `index.ts`.
+Previously was written for InfluxDB 2.0. Take a look into git history if you use Influx instead. Arguably Influx is a better fit for this type of data than Prometheus, but I'm trying something new :)
 
 ## Configuration
 
-Environment Variables:
-
-- `INFLUX_BUCKET`
-- `INFLUX_ORG`
-- `INFLUX_TOKEN`
-- `INFLUX_URL`
+Just deploy it. It'll expose a Prometheus metrics endpoint on 0.0.0.0:8080
 
 ## Docker
 
 The docker build doesn't run the TypeScript compiler or copy the `src` directory, so make sure that you've ran `npm run build` first.
 
 Remember to map UDP port 50222 to the container so that it can listen to the Tempest broadcasting on the LAN.
+Remember to map TCP 8080 to whatever port you want on the host and then configure Prometheus to scrape it
 
 ### Publishing instructions
 
 ```
 npm run build
 docker build . -t kj800x/weatherflow-tempest-sensor
-docker run -p 50222:50222/udp --env-file .env -t kj800x/weatherflow-tempest-sensor
+docker run -p 50222:50222/udp -p 8080:8080/tcp -t kj800x/weatherflow-tempest-sensor
 docker push kj800x/weatherflow-tempest-sensor
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,163 +1,123 @@
-import { InfluxDB, Point } from '@influxdata/influxdb-client';
-import dgram from 'dgram';
-import { ENV } from './env.js';
-import { DecodedEvent, Event, decodeEvent } from './weatherflow.js';
+import client from "prom-client";
+import http from "http";
+import dgram from "dgram";
+import { DecodedEvent, Event, decodeEvent } from "./weatherflow.js";
 
-const server = dgram.createSocket('udp4');
-
-const influx = new InfluxDB({
-  url: ENV('INFLUX_URL'),
-  token: ENV('INFLUX_TOKEN'),
+const metricsServer = http.createServer((__req, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  res.writeHead(200);
+  client.register.metrics().then((s) => {
+    res.end(s);
+  });
 });
 
-const writeApi = influx.getWriteApi(ENV('INFLUX_ORG'), ENV('INFLUX_BUCKET'));
+metricsServer.listen(8080, "0.0.0.0");
+
+const wind_lull = new client.Gauge({ name: "wind_lull", help: "wind_lull" });
+const wind_avg = new client.Gauge({ name: "wind_avg", help: "wind_avg" });
+const wind_gust = new client.Gauge({ name: "wind_gust", help: "wind_gust" });
+const wind_direction = new client.Gauge({
+  name: "wind_direction",
+  help: "wind_direction",
+});
+const wind_sample_interval = new client.Gauge({
+  name: "wind_sample_interval",
+  help: "wind_sample_interval",
+});
+const station_pressure = new client.Gauge({
+  name: "station_pressure",
+  help: "station_pressure",
+});
+const air_temperature = new client.Gauge({
+  name: "air_temperature",
+  help: "air_temperature",
+});
+const relative_humidity = new client.Gauge({
+  name: "relative_humidity",
+  help: "relative_humidity",
+});
+const illuminance = new client.Gauge({
+  name: "illuminance",
+  help: "illuminance",
+});
+const uv = new client.Gauge({ name: "uv", help: "uv" });
+const solar_radiation = new client.Gauge({
+  name: "solar_radiation",
+  help: "solar_radiation",
+});
+const rain_amount = new client.Gauge({
+  name: "rain_amount",
+  help: "rain_amount",
+});
+const precipitation_type = new client.Gauge({
+  name: "precipitation_type",
+  help: "precipitation_type",
+});
+const lighting_count = new client.Gauge({
+  name: "lighting_count",
+  help: "lighting_count",
+});
+const lighting_avg_distance = new client.Gauge({
+  name: "lighting_avg_distance",
+  help: "lighting_avg_distance",
+});
+const battery = new client.Gauge({ name: "battery", help: "battery" });
+const reportInterval = new client.Gauge({
+  name: "reportInterval",
+  help: "reportInterval",
+});
+
+const server = dgram.createSocket("udp4");
 
 async function submitEvent(event: DecodedEvent) {
   // Special casing this because this event might generate multiple points
-  if (event.type === 'obs_st') {
-    const points = event.observations.map((obs) => {
-      const point = new Point(event.type);
-      point.tag('serial_number', event.serial_number);
-      point.tag('hub_sn', event.hub_sn);
-      point.intField('firmware_revision', event.firmwareRevision);
-      point.floatField('wind_lull', obs.windLull);
-      point.floatField('wind_avg', obs.windAvg);
-      point.floatField('wind_gust', obs.windGust);
-      point.intField('wind_direction', obs.windDirection);
-      point.intField('wind_sample_interval', obs.windSampleInterval);
-      point.floatField('station_pressure', obs.stationPressure);
-      point.floatField('air_temperature', obs.temperature);
-      point.floatField('relative_humidity', obs.humidity);
-      point.intField('illuminance', obs.illuminance);
-      point.floatField('uv', obs.uvIndex);
-      point.intField('solar_radiation', obs.solarRadiation);
-      point.floatField('rain_amount', obs.rainAmount);
-      point.stringField('precipitation_type', obs.precipitationType);
-      point.intField('lighting_count', obs.lightningCount);
-      point.floatField('lighting_avg_distance', obs.lightningAvgDistance);
-      point.floatField('battery', obs.battery);
-      point.intField('reportInterval', obs.reportInterval);
-      point.timestamp(new Date(obs.time * 1000));
-      return point;
-    });
-    points.forEach((point) => writeApi.writePoint(point));
-    writeApi.flush();
-    return;
+  if (event.type === "obs_st") {
+    const obs = event.observations[event.observations.length - 1]!;
+    wind_lull.set(obs.windLull);
+    wind_avg.set(obs.windAvg);
+    wind_gust.set(obs.windGust);
+    wind_direction.set(obs.windDirection);
+    wind_sample_interval.set(obs.windSampleInterval);
+    station_pressure.set(obs.stationPressure);
+    air_temperature.set(obs.temperature);
+    relative_humidity.set(obs.humidity);
+    illuminance.set(obs.illuminance);
+    uv.set(obs.uvIndex);
+    solar_radiation.set(obs.solarRadiation);
+    rain_amount.set(obs.rainAmount);
+    precipitation_type.set(
+      (() => {
+        switch (obs.precipitationType) {
+          case "none":
+            return 0;
+          case "rain":
+            return 1;
+          case "hail":
+            return 2;
+        }
+      })()
+    );
+    lighting_count.set(obs.lightningCount);
+    lighting_avg_distance.set(obs.lightningAvgDistance);
+    battery.set(obs.battery);
+    reportInterval.set(obs.reportInterval);
   }
-
-  const point = new Point(event.type);
-  switch (event.type) {
-    case 'rapid_wind': {
-      point.tag('serial_number', event.serial_number);
-      point.tag('hub_sn', event.hub_sn);
-      point.intField('direction', event.direction);
-      point.floatField('speed', event.speed);
-      point.timestamp(new Date(event.time * 1000));
-      break;
-    }
-    case 'evt_precip': {
-      point.tag('serial_number', event.serial_number);
-      point.tag('hub_sn', event.hub_sn);
-      point.timestamp(new Date(event.timestamp * 1000));
-      break;
-    }
-    case 'evt_strike': {
-      point.tag('serial_number', event.serial_number);
-      point.tag('hub_sn', event.hub_sn);
-      point.intField('distance', event.distance);
-      point.intField('energy', event.energy);
-      point.timestamp(new Date(event.time * 1000));
-      break;
-    }
-    case 'hub_status': {
-      point.tag('serial_number', event.serial_number);
-      point.intField(
-        'firmware_revision',
-        parseInt(event.firmware_revision, 10)
-      );
-      point.intField('uptime', event.uptime);
-      point.intField('rssi', event.rssi);
-      point.booleanField('reset-BOR', event.reset_flags.BOR);
-      point.booleanField('reset-PIN', event.reset_flags.PIN);
-      point.booleanField('reset-POR', event.reset_flags.POR);
-      point.booleanField('reset-SFT', event.reset_flags.SFT);
-      point.booleanField('reset-WDG', event.reset_flags.WDG);
-      point.booleanField('reset-WWD', event.reset_flags.WWD);
-      point.booleanField('reset-LPW', event.reset_flags.LPW);
-      point.timestamp(new Date(event.timestamp * 1000));
-      break;
-    }
-    case 'device_status': {
-      point.tag('serial_number', event.serial_number);
-      point.tag('hub_sn', event.hub_sn);
-      point.intField('firmware_revision', event.firmware_revision);
-      point.intField('uptime', event.uptime);
-      point.floatField('voltage', event.voltage);
-      point.intField('rssi', event.rssi);
-      point.intField('hub_rssi', event.hub_rssi);
-      point.booleanField('sensorStatus-ok', event.sensor_status.sensors_ok);
-      point.booleanField(
-        'sensorStatus-lightingFailed',
-        event.sensor_status.lightning_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-lightingNoise',
-        event.sensor_status.lightning_sensor_noise
-      );
-      point.booleanField(
-        'sensorStatus-lightingDisturbance',
-        event.sensor_status.lightning_sensor_disturbance
-      );
-      point.booleanField(
-        'sensorStatus-pressureFailed',
-        event.sensor_status.pressure_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-temperatureFailed',
-        event.sensor_status.temperature_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-humidityFailed',
-        event.sensor_status.humidity_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-windFailed',
-        event.sensor_status.wind_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-precipitationFailed',
-        event.sensor_status.precipitation_sensor_failed
-      );
-      point.booleanField(
-        'sensorStatus-lightUvFailed',
-        event.sensor_status.light_uv_sensor_failed
-      );
-      point.booleanField('debug_enabled', event.debug_enabled);
-      point.timestamp(new Date(event.timestamp * 1000));
-      break;
-    }
-    default: {
-      throw new Error(`Unknown event type: ${(event as any).type}`);
-    }
-  }
-  writeApi.writePoint(point);
-  writeApi.flush();
 }
 
-server.on('error', (err) => {
+server.on("error", (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
+server.on("message", (msg, rinfo) => {
   console.log(`received message from ${rinfo.address}:${rinfo.port}: ${msg}`);
 
-  const rawMessage: Event = JSON.parse(msg.toString('utf8'));
+  const rawMessage: Event = JSON.parse(msg.toString("utf8"));
   const decoded = decodeEvent(rawMessage);
   submitEvent(decoded);
 });
 
-server.on('listening', () => {
+server.on("listening", () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
