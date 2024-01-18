85104c600f0a1e62ff66fe625062590c6d3c4b73
Kevin Johnson <kevin@coolkev.com>
2024-01-18 10:06:11 -0500

    Remove comments

--- src/metricWatchdog.ts
+++ src/metricWatchdog.ts
@@ -1,6 +1,7 @@
 import client from "prom-client";
 
 const TWO_MINUTES_IN_MS = 2 * 60 * 1000;
+const WATCHDOG_CHECK_INTERVAL = 500;
 
 export function withGaugeWatchdog<T extends string>(
   metric: client.Gauge<T>,
@@ -11,29 +12,18 @@ export function withGaugeWatchdog<T extends string>(
   setInterval(() => {
     for (const key of lastSeenMap.keys()) {
       if (lastSeenMap.get(key)!.getTime() + timeout < new Date().getTime()) {
-        // console.log(
-        //   "watchdog remove",
-        //   // @ts-expect-error private for debugging
-        //   metric.name,
-        //   "with labels",
-        //   JSON.parse(key)
-        // );
         lastSeenMap.delete(key);
         metric.remove(JSON.parse(key));
       }
     }
-  }, 500);
+  }, WATCHDOG_CHECK_INTERVAL);
 
   return {
     set(labels: Record<T, string | number>, value: number) {
-      // // @ts-expect-error private for debugging
-      // console.log("seen", metric.name, "with labels", labels);
       lastSeenMap.set(JSON.stringify(labels), new Date());
       metric.set(labels, value);
     },
     remove(labels: Record<T, string | number>) {
-      // // @ts-expect-error private for debugging
-      // console.log("manual remove", metric.name, "with labels", labels);
       lastSeenMap.delete(JSON.stringify(labels));
       metric.remove(labels);
     },
